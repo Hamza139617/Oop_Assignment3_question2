d56b75b3911fdbc2e3d3611d7507f96a122bc943
Hamza <hamza139617@gmail.com>
2026-04-16 07:39:35 -0700

    worked on the base class of the game , took time because it was very hard to understand the structure

--- Oop_Assignment3_question2/Kingdom.h
+++ Oop_Assignment3_question2/Kingdom.h
@@ -1,1 +1,146 @@
 #pragma once
+
+// Forward Declarations
+
+class LandedLord;
+class LandlessLord;
+class FootSoldier;
+class Knight;
+class Cavalry;
+class BatteringRam;
+class Catapult;
+class Warship;
+class WarriorLord;
+class Sage;
+
+
+
+class Kingdom {
+	
+protected:
+	int wealth;
+	float defenseStat;
+
+
+private:
+
+
+public:
+
+
+};
+
+class FrostPeaksKingdom: public Kingdom {
+
+private:
+	int coldAttritionModifier; // default value 5 
+
+public:
+	void applyColdWeather();
+
+};
+
+class ThePeaksOfFrost : public FrostPeaksKingdom {
+
+private:
+	
+
+public:
+	
+
+};
+
+class VerdantKingdom : public Kingdom {
+
+private:
+	int foodSurplusBonus;
+
+public:
+	void applyFoodBonus();
+
+};
+
+class TheSkyCitadel : public VerdantKingdom {
+
+private:
+
+public:
+};
+
+
+class TheVerdantExpanse : public VerdantKingdom {
+
+private:
+
+public:
+
+};
+
+class TheCrimsonSands : public VerdantKingdom {
+
+private:
+
+public:
+
+
+};
+
+class TheAzureRiverlands : public VerdantKingdom {
+
+private:
+
+public:
+
+
+};
+
+class CoastalKingdom : public Kingdom {
+
+private:
+	int navalTradeBonus;
+public:
+	void applyTradeBonus();
+
+};
+
+class TheSunlandEmpire : public CoastalKingdom {
+
+private:
+
+public:
+
+
+};
+
+
+class  TheCorsairIsles : public CoastalKingdom {
+
+
+private:
+
+public:
+
+
+};
+
+class TheTempestCoast : public CoastalKingdom {
+
+private:
+
+public:
+
+
+};
+
+
+class TheImperialSeat : public CoastalKingdom {
+
+private:
+
+public:
+
+
+};
+
+// will be working on the unique realms later 
+
